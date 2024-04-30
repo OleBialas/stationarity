7a95f951bba4b454277e44ba443bd1124ea38a86
Ole <ole.bialas@posteo.de>
2024-04-30 16:11:41 +0200

    [DATALAD] Recorded changes

---
+++ code/powerlaw_noise_sim.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+import json
+import numpy as np
+from mtrf import TRF
+from generators import wavelet, random_pulses, power_law_noise, scale_noise
+from utils import segment_data
+
+root = Path(__file__).parent.parent.absolute()
+p = json.load(open(root / "code" / "parameters.json"))
+np.random.seed(p["seed"])
+dur_segment = [1, 2, 5, 10, 30, 60, 100, 200]
+n_reps = 100
+tmin, tmax = 0, 0.25
+snr_db = -30
+
+# simulate stimuli and neural response
+tf, tf_times = wavelet(tmin, tmax, p["fs"], 4, 0.1, 0.03)
+stim_train, stim_test = [
+    random_pulses(p["dur_sim"], p["fs"], 8, 0.005, 0.04, 1, 5) for _ in range(2)
+]
+stim_times = np.linspace(0, len(stim_train) / p["fs"], len(stim_train))
+resp_train = np.convolve(stim_train, tf, mode="full")
+resp_test = np.convolve(stim_test, tf, mode="full")
+resp_train, resp_test = resp_train[: len(stim_train)], resp_test[: len(stim_test)]
+stim_train = np.expand_dims(stim_train, axis=1)
+stim_test = np.expand_dims(stim_test, axis=1)
+resp_train = np.expand_dims(resp_train, axis=1)
+resp_test = np.expand_dims(resp_test, axis=1)
+
+acc, reg = [np.zeros((n_reps, 2, len(dur_segment))) for _ in range(2)]
+for i_r in range(n_reps):
+    print(i_r)
+    for i_a, alpha in enumerate([0, 2]):  # use white and pink noise
+        noise = scale_noise(
+            resp_train, power_law_noise(p["dur_sim"], p["fs"], alpha), snr_db
+        )
+        noise = np.expand_dims(noise, axis=1)
+        for i_d, dur in enumerate(dur_segment):
+            stim_train_segments, resp_train_segments = segment_data(
+                [stim_train], [resp_train + noise], dur, normalize=True
+            )
+            stim_test_segments, resp_test_segments = segment_data(
+                [stim_test], [resp_test], dur, normalize=True
+            )
+
+            trf = TRF()
+            trf.train(
+                stim_train_segments,
+                resp_train_segments,
+                p["fs"],
+                tmin,
+                tmax,
+                p["lambda"],
+                k=p["cv_folds"],
+                seed=p["seed"],
+                verbose=False,
+            )
+
+            reg[i_r, i_a, i_d] = trf.regularization
+            acc[i_r, i_a, i_d] = trf.predict(stim_test_segments, resp_test_segments)[-1]
+np.save(root / "results" / "white_vs_pink_noise_sim_lambda.npy", reg)
+np.save(root / "results" / "white_vs_pink_noise_sim_accuracy.npy", acc)
