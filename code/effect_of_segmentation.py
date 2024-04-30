d3572b2e535448ef11325529c06bbbfef613b296
Ole <ole.bialas@posteo.de>
2024-04-30 14:06:45 +0200

    [DATALAD] Recorded changes

--- code/effect_of_segmentation.py
+++ code/effect_of_segmentation.py
@@ -10,7 +10,7 @@ Writes:
       subjects by segment duration by number of spectral bands. With the optimal
       value of lambda
 """
-
+from argparse import ArgumentParser
 from pathlib import Path
 import json
 import numpy as np
@@ -22,16 +22,11 @@ root = Path(__file__).parent.parent.absolute()
 p = json.load(open(root / "code" / "parameters.json"))
 np.random.seed(p["seed"])
 
-subjects = list((root / "data").glob("sub*"))
-subjects.sort()
 
-accuracy, regularization = [
-    np.zeros((len(subjects), len(p["dur_segment"]), len(p["stim"]["n_bands"])))
-    for _ in range(2)
-]
-
-for i_s, sub in enumerate(subjects):
-    print(f"Preparing recordings for {sub.name} ...")
+def effect_of_segmentation(sub):
+    accuracy, regularization = [
+        np.zeros((len(p["dur_segment"]), len(p["stim"]["n_bands"]))) for _ in range(2)
+    ]
     response = load_eeg(sub)
     for i_b, bands in enumerate(p["stim"]["n_bands"]):
         print(f"Computing spectrogram with {bands} bands ...")
@@ -52,7 +47,16 @@ for i_s, sub in enumerate(subjects):
                 k=p["cv_folds"],
                 verbose=False,
             ).max()
-            accuracy[i_s, i_d, i_b] = r
-            regularization[i_s, i_d, i_b] = trf.regularization
-    np.save(root / "results" / "fit" / f"{sub.name}_accuracy.npy", accuracy)
-    np.save(root / "results" / "fit" / f"{sub.name}_lambda.npy", regularization)
+            accuracy[i_d, i_b] = r
+            regularization[i_d, i_b] = trf.regularization
+        np.save(root / "results" / "fit" / f"{sub.name}_accuracy.npy", accuracy)
+        np.save(root / "results" / "fit" / f"{sub.name}_lambda.npy", regularization)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="Fit forward TRF with variable data segment duration. Saves files {sub}_accuracy.npy and {sub}_lamba.npy in /results/fit."
+    )
+    parser.add_argument("sub", type=str, help="subject ID, e.g. 'sub-004'")
+    args = parser.parse_args()
+    effect_of_segmentation(args.sub)
---
+++ code/effect_of_segmentation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Submit the job array with one job per subject and number of bands
+#SBATCH --output=/scratch/obialas/logs/seg_%A_%a.log
+#SBATCH --array=1-20
+#SBATCH -t 0-48:00:00
+#SBATCH -c 4 --mem-per-cpu=6G
+#SBATCH --partition=standard
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ole.bialas@posteo.de
+
+# set up the environment
+source /software/anaconda/2019.10/bin/activate hivemind
+cd /scratch/obialas/stationarity
+
+# Extract parameters from the json file
+json_file="code/parameters.json"
+sub_list=$(jq -r '.subjects | @csv' "$json_file")
+
+# Convert the CSV string to an array
+IFS=',' read -ra sub_array <<< "$sub_list"
+
+# remove quotation marks
+sub=$(echo "$sub" | sed 's/^"\(.*\)"$/\1/')
+
+echo "running $sub"
+
+# create throw-away clone in temporay directory
+cd /local_scratch/$SLURM_JOB_ID
+datalad clone /scratch/obialas/stationarity ds
+cd ds
+mkdir -p results/fit
+git-annex dead here # dont use git annex in the clone
+git checkout -b "job-$SLURM_ARRAY_TASK_ID" # new unique branch
+
+datalad run --input "data/$sub" --input "results/spectrogram" --output "results/fit/*" "python code/effect_of_segmentation.py $sub"
+datalad push --to origin
+
